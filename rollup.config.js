54817c5efb32031e30ecb645678a75b09a53206c
Spencer Stolworthy <sdstolworthy@gmail.com>
2019-10-25 10:46:12 -0600

    added minifier to package

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from "rollup-plugin-commonjs";
 import external from "rollup-plugin-peer-deps-external";
 import resolve from "rollup-plugin-node-resolve";
 import replace from "rollup-plugin-replace";
+import { uglify } from "rollup-plugin-uglify";
 
 import pkg from "./package.json";
 
@@ -39,6 +40,7 @@ export default {
     }),
     replace({
       "process.env.NODE_ENV": JSON.stringify("production")
-    })
+    }),
+    uglify({ compress: { drop_console: true, drop_debugger: true } })
   ]
 };
