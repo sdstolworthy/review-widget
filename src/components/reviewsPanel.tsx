3c6866ef9589223e78c3b159529aec7bdadd7520
Spencer Stolworthy <sdstolworthy@gmail.com>
2019-10-25 14:19:36 -0600

    added header to review block

--- src/components/reviewsPanel.tsx
+++ src/components/reviewsPanel.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { DIReview } from "../createApp";
 import ReviewBox from "./reviewBox";
+import Rating from "react-rating";
 
 interface IProps {
   reviews: Array<DIReview>;
@@ -16,16 +17,51 @@ export default function Reviews(props: IProps) {
     return () => window.removeEventListener("resize", handleResize);
   });
   return (
-    <div
-      style={{
-        columnCount: Math.floor(width / 320),
-        gridGap: "1em",
-        padding: "1em",
-      }}
-    >
-      {props.reviews.map(review => {
-        return <ReviewBox review={review} />;
-      })}
+    <div style={{ width: "100%" }}>
+      <div
+        style={{
+          padding: "1em",
+          width: "calc(100% - 2em)",
+          borderBottom: "1px #DEDEDE solid",
+          display: "flex",
+          alignItems: "center"
+        }}
+      >
+        <div style={{ display: "inline-block" }}>
+          <Rating
+            readonly={true}
+            initialRating={
+              (props.reviews
+                .filter(r => typeof r.rating_max === "number" && r.rating)
+                .reduce((a, b) => a + b.rating / b.rating_max, 0) /
+                props.reviews.length) *
+              5
+            }
+            stop={5}
+            start={0}
+          />
+        </div>
+        <h2
+          style={{
+            display: "inline",
+            textAlign: "center",
+            verticalAlign: "middle"
+          }}
+        >
+          &nbsp;&nbsp;{props.reviews.length} reviews
+        </h2>
+      </div>
+      <div
+        style={{
+          columnCount: Math.floor(width / 320),
+          gridGap: "1em",
+          padding: "1em"
+        }}
+      >
+        {props.reviews.map(review => {
+          return <ReviewBox review={review} />;
+        })}
+      </div>
     </div>
   );
 }
