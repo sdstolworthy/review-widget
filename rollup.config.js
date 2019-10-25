acae107edfe75627b0008133ee63a2bffd035191
Spencer Stolworthy <sdstolworthy@gmail.com>
2019-10-25 14:38:44 -0600

    added uglify

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import commonjs from "rollup-plugin-commonjs";
 import external from "rollup-plugin-peer-deps-external";
 import resolve from "rollup-plugin-node-resolve";
 import replace from "rollup-plugin-replace";
-// import { uglify } from "rollup-plugin-uglify";
+import { uglify } from "rollup-plugin-uglify";
 
 import pkg from "./package.json";
 
@@ -44,6 +44,6 @@ export default {
     replace({
       "process.env.NODE_ENV": JSON.stringify("production")
     }),
-    // uglify({ compress: { drop_console: true, drop_debugger: true } })
+    uglify({ compress: { drop_console: true, drop_debugger: true } })
   ]
 };
